163e915e33d36d8324a61e22c6a935dcb0edc7b3
Ellie Salyer <47187853+lilgatr@users.noreply.github.com>
2020-10-30 09:17:26 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Calendar from './app/screens/calendar';
+import Calendaar from './app/screens/calendar';
 import Title from './app/screens/title';
 import MyPlants from './app/screens/myPlants';
 import PlantCard from './app/screens/plantCard';
@@ -9,6 +9,6 @@ import SignIn from './app/screens/signIn';
 import SignUp from './app/screens/signUp';
 import Today from './app/screens/today';
 
-const App = () => <Calendar />;
+const App = () => <Calendaar />;
 
 export default App;
