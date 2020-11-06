df98a895bab1f18e5d6e0d21ae57155496eb0943
Eli <elliasalyer@gmail.com>
2020-11-05 20:52:57 -0500

    started today page

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import Calendaar from './app/screens/calendar';
-import Title from './app/screens/title';
-import MyPlants from './app/screens/myPlants';
-import PlantCard from './app/screens/plantCard';
-import Settings from './app/screens/settings';
-import SignIn from './app/screens/signIn';
-import SignUp from './app/screens/signUp';
-import Today from './app/screens/today';
+import Calendaar from "./app/screens/calendar";
+import Title from "./app/screens/title";
+import MyPlants from "./app/screens/myPlants";
+import PlantCard from "./app/screens/plantCard";
+import Settings from "./app/screens/settings";
+import SignIn from "./app/screens/signIn";
+import SignUp from "./app/screens/signUp";
+import Today from "./app/screens/today";
 
-const App = () => <Calendaar />;
+const App = () => <Today />;
 
 export default App;
--- app/screens/today.js
+++ app/screens/today.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {
   StyleSheet,
-  Text,
   View,
   SafeAreaView,
   SectionList,
@@ -9,7 +8,7 @@ import {
   Image,
   Button,
 } from "react-native";
-import { Calendar, CalendarList, Agenda } from "react-native-calendars";
+import { Divider, Text } from "react-native-paper";
 import { Constants } from "expo";
 
 const Today = () => {
@@ -17,96 +16,56 @@ const Today = () => {
     <SafeAreaView style={styles.container}>
       <View style={styles.btncontainer}>
         <TouchableOpacity style={[styles.round, styles.leftbtn]}>
-          <Text style={{ color: "black", fontSize: 13, fontWeight: "bold" }}>
+          <Text style={{ color: "#FFF", fontSize: 13, fontWeight: "bold" }}>
             Today
           </Text>
         </TouchableOpacity>
         <TouchableOpacity style={[styles.round, styles.rightbtn]}>
-          <Text style={{ color: "#fff", fontSize: 13, fontWeight: "bold" }}>
+          <Text style={{ color: "black", fontSize: 13, fontWeight: "bold" }}>
             Calendar
           </Text>
         </TouchableOpacity>
+      </View>
+      <View style={styles.container}>
+        <Text>Nov 1, 2020</Text>
       </View>
     </SafeAreaView>
   );
 };
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     marginTop: 50,
-//   },
-//   btncontainer: {
-//     alignSelf: "center",
-//     flexDirection: "row",
-//     paddingVertical: 16,
-//   },
-//   leftbtn: {
-//     fontSize: 13,
-//     fontWeight: "bold",
-//     //fontColor: "#352F39",
-//     backgroundColor: "#fff",
-//     marginTop: 10,
-//     padding: 12,
-//     //flexDirection: "row",
-//     //alignItems: "center",
-//     borderColor: "#352F39",
-//     borderRadius: 16,
-//     borderWidth: 2,
-
-//     //borderBottomWidth: 4,
-//   },
-//   rightbtn: {
-//     fontSize: 13,
-//     fontWeight: "bold",
-
-//     //paddingLeft: 16,
-//     backgroundColor: "#352F39",
-//     marginTop: 10,
-//     padding: 12,
-//     //flexDirection: "row",
-//     //alignItems: "center",
-//     borderColor: "#352F39",
-//     borderRadius: 16,
-//     borderWidth: 2,
-
-//     paddingLeft: 50,
-//   },
-//   cal: {},
-// });
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 50,
+    paddingHorizontal: 16,
   },
   btncontainer: {
-    flex: 1,
     paddingBottom: 24,
   },
   leftbtn: {
-    backgroundColor: "red",
+    backgroundColor: "#352F39",
     position: "absolute",
     top: 20,
     left: 100,
     zIndex: 2,
   },
-
   rightbtn: {
-    backgroundColor: "green",
+    backgroundColor: "#FFF",
     position: "absolute",
     top: 20,
     left: 190,
     borderTopLeftRadius: 0,
     borderBottomLeftRadius: 0,
   },
-
   round: {
     justifyContent: "center",
     alignItems: "center",
     width: 110,
     height: 50,
-    borderRadius: 50,
+    borderRadius: 16,
+    borderWidth: 2,
+    borderColor: "#352F39",
+    borderBottomWidth: 4,
   },
 });
 
