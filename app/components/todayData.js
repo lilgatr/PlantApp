9fc76ae767d3cb578952bb0e6b3c632a2c3bd8c2
Eli Salyer <mad@dbcs-a.k12k.com>
2020-12-03 12:01:34 -0500

    created todayData

---
+++ app/components/todayData.js
@@ -0,0 +1,14 @@
+const todayData = [
+  {
+    id: "1",
+    title: "First Bird",
+    subtitle: "1st sub",
+  },
+  {
+    id: "2",
+    title: "Second Bird",
+    subtitle: "2nd sub",
+  },
+];
+
+export default todayData;
